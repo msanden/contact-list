ba9332238472569978de92d3ad81ccf873b091f1
Manuel Sande <mnlsande22@gmail.com>
2019-04-01 03:10:54 -0400

    create contact class

---
+++ contact.py
@@ -0,0 +1,9 @@
+class Contact:
+    """
+    class that creates new instances of a contact
+    """
+    def __init__(self,firstName,lastName,phoneNumber,email):
+        self.firstName = firstName;
+        self.lastName = lastName;
+        self.phoneNumber = phoneNumber;
+        self.email = email;
