be26d224616127a166fd1eac687b1a8f5b173e39
Manuel Sande <mnlsande22@gmail.com>
2019-04-01 04:47:25 -0400

    Create save_contact test case, Reset ContactList[] after each test

--- contact_test.py
+++ contact_test.py
@@ -9,9 +9,14 @@ class TestContact(unittest.TestCase):
     """
 
     def setUp(self):
-        """ Set up method to run before each test case."""
+        """ setUp() method to run before each test case."""
 
         self.new_contact = Contact("Manuel","Sande","3153806748","axl@glam.com");
+
+    def tearDown(self):
+        """tearDown() method that runs after each test case."""
+
+        Contact.contactList = [];
 
     def test_init(self):
         """test_init() test case to test if our object is being instantiated
@@ -21,6 +26,33 @@ class TestContact(unittest.TestCase):
         self.assertEqual(self.new_contact.lastName,"Sande")
         self.assertEqual(self.new_contact.phoneNumber,"3153806748")
         self.assertEqual(self.new_contact.email,"axl@glam.com")
+
+    def test_save_contact(self):
+        """test_save_contact() test case to test if contact object is saved
+        to contact list"""
+
+        self.new_contact.save_contact()
+        self.assertEqual(len(Contact.contactList),1)
+
+    def test_save_multiple_contact(self):
+        """method test_save_multiple_contact() checks if we can save multiple
+        contact objects to our contactList[]"""
+
+        self.new_contact.save_contact()
+        test_contact = Contact("Ednas","Nell","3806743158","axy@fab.com");
+        test_contact.save_contact()
+        self.assertEqual(len(Contact.contactList),2)
+
+    def test_delete_contact(self):
+        """test_delete_contact() tests if we can delete a contact from our
+        contact list"""
+
+        self.new_contact.save_contact()
+        test_contact = Contact("Ednas","Nell","3806743158","axy@fab.com");
+        test_contact.save_contact()
+
+        self.new_contact.delete_contact()
+        self.assertEqual(len(Contact.contactList),1)
 
 if __name__ == '__main__':
     unittest.main()
