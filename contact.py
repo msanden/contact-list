27dc2b90f708f0e65a2d5a0b6f27dbaa94202113
Manuel Sande <mnlsande22@gmail.com>
2019-04-01 04:47:57 -0400

    Add save_contact, delete_contact methods

--- contact.py
+++ contact.py
@@ -2,8 +2,19 @@ class Contact:
     """
     class that creates new instances of a contact
     """
+    contactList = [];
+
     def __init__(self,firstName,lastName,phoneNumber,email):
         self.firstName = firstName;
         self.lastName = lastName;
         self.phoneNumber = phoneNumber;
         self.email = email;
+
+    def save_contact(self):
+        """
+        save_contact() method saves contacts to contactList
+        """
+        Contact.contactList.append(self);
+
+    def delete_contact(self):
+        Contact.contactList.remove(self);
